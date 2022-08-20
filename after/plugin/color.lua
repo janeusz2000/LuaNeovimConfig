b285aa76ee12ae019a3edd3f6404f4bf80763974
Animator <mateusz.krzysztof.janik@gmail.com>
2022-08-20 14:24:00 +0200

    pure Neovim setup

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -3,3 +3,14 @@ vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
 
 vim.cmd("colorscheme tokyonight")
+
+
+-- Custom colored groups
+vim.api.nvim_set_hl(0, "Comment", { fg = "#009933" })
+
+-- Line Numbers colors
+local relativeLineColor = "#FCAD03"
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#03FCF4" })
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = relativeLineColor })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = relativeLineColor })
+vim.api.nvim_set_hl(0, "Visual", {reverse = true})
