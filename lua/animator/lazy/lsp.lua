c559e3478430f68137e1147bbad6431efe706001
janeusz2000 <janeusz2000@gmail.com>
2025-12-27 02:48:36 +0100

    fixing clangd on macos

--- lua/animator/lazy/lsp.lua
+++ lua/animator/lazy/lsp.lua
@@ -57,54 +57,78 @@ return {
 
         require("fidget").setup({})
         require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
-                "clangd",
-                "lua_ls"
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities,
-                        on_attach = on_attach
-                    }
-                end,
+        local mason_lspconfig = require("mason-lspconfig")
+        local handlers = {
+            function(server_name) -- default handler (optional)
+                require("lspconfig")[server_name].setup {
+                    capabilities = capabilities,
+                    on_attach = on_attach
+                }
+            end,
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
+            zls = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.zls.setup({
+                    root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                    settings = {
+                        zls = {
+                            enable_inlay_hints = true,
+                            enable_snippets = true,
+                            warn_style = true,
                         },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
+                    },
+                })
+                vim.g.zig_fmt_parse_errors = 0
+                vim.g.zig_fmt_autosave = 0
 
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
+            end,
+            ["lua_ls"] = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.lua_ls.setup {
+                    capabilities = capabilities,
+                    settings = {
+                        Lua = {
+                            runtime = { version = "Lua 5.1" },
+                            diagnostics = {
+                                globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
                             }
                         }
                     }
-                end,
-            }
-        })
+                }
+            end,
+        }
+
+        local ensure_installed = {
+            "lua_ls",
+            "rust_analyzer",
+            "gopls",
+            "lua_ls"
+        }
+
+        if mason_lspconfig.setup_handlers then
+            mason_lspconfig.setup({ ensure_installed = ensure_installed })
+            mason_lspconfig.setup_handlers(handlers)
+        else
+            mason_lspconfig.setup({
+                ensure_installed = ensure_installed,
+                handlers = handlers
+            })
+        end
+
+        if vim.lsp.config and vim.lsp.enable then
+            vim.lsp.config("clangd", {
+                cmd = { "/Library/Developer/CommandLineTools/usr/bin/clangd" },
+                capabilities = capabilities,
+                on_attach = on_attach
+            })
+            vim.lsp.enable("clangd")
+        else
+            require("lspconfig").clangd.setup({
+                cmd = { "/Library/Developer/CommandLineTools/usr/bin/clangd" },
+                capabilities = capabilities,
+                on_attach = on_attach
+            })
+        end
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
